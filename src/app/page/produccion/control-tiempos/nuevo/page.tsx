8f7c2fc1de8d98b855eb5aa2d348239dfe36d021
itcoordinator1n <anibal.reyes@infarma.hn>
2026-04-24 04:10:45 -0600

    i

--- src/app/page/produccion/control-tiempos/nuevo/page.tsx
+++ src/app/page/produccion/control-tiempos/nuevo/page.tsx
@@ -562,7 +562,7 @@ export default function NuevoControlTiempos() {
                       {(() => {
                         let globalMs = 0;
                         const renderedIds = new Set<string>();
-                        const rows: JSX.Element[] = [];
+                        const rows = [];
 
                         // 1. Renderizar actividades del formato
                         FORMATO_ACTIVIDADES.forEach((item, i) => {
